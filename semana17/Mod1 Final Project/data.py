a9e36de439cd2ab384df16f8e8a65100da37b5ad
Joced Nieves <joced.alex@gmail.com>
2025-01-09 22:54:47 -0600

    Final project v1.1 optimization

--- semana17/Mod1 Final Project/data.py
+++ semana17/Mod1 Final Project/data.py
@@ -1,28 +1,25 @@
 import csv
 
 def save_data(data, file_name):
-
-    if file_name=='movements':
-        with open(f'my_test_repo/semana17/Mod1 Final Project/{file_name}.csv', mode='w', newline='') as file:
+    
+    with open(f'my_test_repo/semana17/Mod1 Final Project/{file_name}.csv', mode='w', newline='') as file:
+        
+        if file_name=='movements':
             writer = csv.DictWriter(file,('Title', 'Type', 'Category', 'Amount'))
             writer.writeheader()
 
             for item in data:
                 writer.writerow({'Title': item[0], 'Type': item[1], 'Category': item[2], 'Amount': item[3]})
-                       
 
-        print('Data saved successfully')
-
-    elif file_name=='categories':
-        with open(f'my_test_repo/semana17/Mod1 Final Project/{file_name}.csv', mode='w', newline='') as file:
+        elif file_name=='categories':
             writer = csv.DictWriter(file,('Name',))
             writer.writeheader()
 
             for item in data:
                 writer.writerow({'Name': item})
-            
-
+                       
         print('Data saved successfully')
+
 
 def import_data(file_name):
     
